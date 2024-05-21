5398e3d4194b7fd352c94f6f6a46601da2ee9e36
Zhannurkhan <zhannurkhan04@gmail.com>
2024-05-21 09:44:40 +0500

    first commit

---
+++ src/database/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: "postgres",
+                host: configService.get<string>("DATABASE_HOST"),
+                port: configService.get<number>("DATABASE_PORT"),
+                username: configService.get<string>("DATABASE_USER"),
+                password: configService.get<string>("DATABASE_PASS"),
+                database: configService.get<string>("DATABASE_NAME"),
+                autoLoadEntities: true,
+                synchronize: true,
+            }),
+        }),
+    ],
+})
+export class DatabaseModule {}
